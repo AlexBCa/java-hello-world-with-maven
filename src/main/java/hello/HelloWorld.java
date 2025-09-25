3597cc472cf432979635c9fcab485b08706bfb93
AlexBCa <alexbeca92@gmail.com>
2025-09-25 13:22:46 +0200

    feat: 106 2

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,19 +2,19 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Current time:");
-        System.out.println(java.time.LocalTime.now());
+        System.out.println("Hello!");
+        System.out.println("Starting operations...");
+        
+        java.lang.Math.addExact(1, 2);
+        java.lang.Math.addExact(10, 35);
+        java.lang.Math.addExact(-5, 3);
+        java.lang.Math.addExact(1_000_000_000, 1);           
+        java.lang.Math.addExact(1_000_000_000L, 2_000_000_000L); 
+        java.lang.Integer.sum(7, 8);
+        java.lang.Long.sum(100L, 200L);
+        java.util.stream.IntStream.of(1, 2, 3, 4).sum();
+        java.util.stream.LongStream.of(5L, 6L, 7L).sum();
 
-        System.out.println("Greeting:");
-        System.out.println(new Greeter().sayHello());
-
-        System.out.println("Args count:");
-        System.out.println(args == null ? 0 : args.length);
-
-        System.out.println("Java version:");
-        System.out.println(System.getProperty("java.version"));
-
-        System.out.println("Thread name:");
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("Operations finished.");
     }
 }
