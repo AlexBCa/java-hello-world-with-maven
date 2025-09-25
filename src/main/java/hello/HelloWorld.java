dd8e1a619f552acfd6f2659519c1259c3c5cdbb0
AlexBCa <alexbeca92@gmail.com>
2025-09-25 13:25:46 +0200

    feat: 106 2

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,18 +3,9 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("Starting operations...");
-        
-        java.lang.Math.addExact(1, 2);
-        java.lang.Math.addExact(10, 35);
-        java.lang.Math.addExact(-5, 3);
-        java.lang.Math.addExact(1_000_000_000, 1);           
-        java.lang.Math.addExact(1_000_000_000L, 2_000_000_000L); 
-        java.lang.Integer.sum(7, 8);
-        java.lang.Long.sum(100L, 200L);
-        java.util.stream.IntStream.of(1, 2, 3, 4).sum();
-        java.util.stream.LongStream.of(5L, 6L, 7L).sum();
-
-        System.out.println("Operations finished.");
+        System.out.println("How are you?");
+        System.out.println("I'm fine, thank you!");
+        System.out.println("What about you?");
+        System.out.println("I'm doing well, thanks for asking!");
     }
 }
